5ce27352de08c05674fd2df1a733d445c97c42ea
vishwaskalani <vishwaskalani2002@gmail.com>
2022-03-08 19:40:12 +0530

    gaussian

---
+++ noise.py
@@ -0,0 +1,14 @@
+import numpy
+def estimate_noise(I):
+
+  H, W = I.shape
+
+  M = [[1, -2, 1],
+       [-2, 4, -2],
+       [1, -2, 1]]
+
+  sigma = np.sum(np.sum(np.absolute(convolve2d(I, M))))
+  sigma = sigma * math.sqrt(0.5 * math.pi) / (6 * (W-2) * (H-2))
+
+  return sigma
+

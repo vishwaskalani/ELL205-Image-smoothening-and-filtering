ee8aae89be39b4d5c528b671ae4f50d8e962514a
vedantdeo <vedantdeo1@gmail.com>
2022-03-21 18:14:31 +0530

    Template ready for LOG

--- boxfilters.py
+++ boxfilters.py
@@ -5,16 +5,20 @@ def filters(imagefile):
 	img1 = cv2.imread(imagefile)
 	dblur=convolution.filter(img1,"Delta",3)
 	gblur =convolution.filter(img1,"Gaussian",3,456)
+	logblur =convolution.filter(img1,"LoG",3,456)
 	mblur=convolution.filter(img1,"Mean",3)
-	cv2.imwrite(imagefile+"_delta.jpg",dblur)
-	cv2.imwrite(imagefile+"_gaussian.jpg",gblur)
-	cv2.imwrite(imagefile+"_mean.jpg",mblur)
+	imagename=imagefile.split(".")[0]
+	cv2.imwrite(imagename+"_delta.jpg",dblur)
+	cv2.imwrite(imagename+"_gaussian.jpg",gblur)
+	cv2.imwrite(imagename+"_mean.jpg",mblur)
+	cv2.imwrite(imagename+"_log.jpg",logblur)	
 	print(gblur.shape[0],gblur.shape[1])
 
 	print(estimate_sigma(img1, channel_axis=-1, average_sigmas=True))
 	print(estimate_sigma(dblur, channel_axis=-1, average_sigmas=True))
 	print(estimate_sigma(gblur, channel_axis=-1, average_sigmas=True))
 	print(estimate_sigma(mblur, channel_axis=-1, average_sigmas=True))
+	print(estimate_sigma(logblur, channel_axis=-1, average_sigmas=True))
 filters("1.jpg")
 
 
--- plots.py
+++ plots.py
@@ -1,28 +1,35 @@
 from matplotlib import pyplot as plt
 import cv2
 import numpy as np
+def plot(imagefile):
+	def RGBify(tensor):
+		rows = tensor.shape[0]
+		cols = tensor.shape[1]
+		r = np.zeros(shape=(rows, cols))
+		g = np.zeros(shape=(rows, cols))
+		b = np.zeros(shape=(rows, cols))
+		for i in range(rows):
+			for j in range(cols):
+				r[i][j] = tensor[i][j][0]
+				g[i][j] = tensor[i][j][1]
+				b[i][j] = tensor[i][j][2]
+		return cv2.merge((b,g,r))
+	imagename=imagefile.split('.')[0]
+	img1=RGBify(cv2.imread(imagefile))
+	dblur=RGBify(cv2.imread(imagename+"_delta.jpg"))
+	gblur=RGBify(cv2.imread(imagename+"_gaussian.jpg"))
+	logblur=RGBify(cv2.imread(imagename+"_log.jpg"))
+	mblur=RGBify(cv2.imread(imagename+"_mean.jpg"))
 
-def RGBify(tensor):
-	rows = tensor.shape[0]
-	cols = tensor.shape[1]
-	r = np.zeros(shape=(rows, cols))
-	g = np.zeros(shape=(rows, cols))
-	b = np.zeros(shape=(rows, cols))
-	for i in range(rows):
-		for j in range(cols):
-			r[i][j] = tensor[i][j][0]
-			g[i][j] = tensor[i][j][1]
-			b[i][j] = tensor[i][j][2]
-	return cv2.merge((b,g,r))
-    
-img1=RGBify(cv2.imread("1.jpg"))
-blur=RGBify(cv2.imread("output.jpg"))
-gblur=RGBify(cv2.imread("outputg.jpg"))
-
-plt.subplot(131),plt.imshow(img1.astype('uint8')),plt.title('Original')
-plt.xticks([]), plt.yticks([])
-plt.subplot(132),plt.imshow(blur.astype('uint8')),plt.title('Blurred')
-plt.xticks([]), plt.yticks([])
-plt.subplot(133),plt.imshow(gblur.astype('uint8')),plt.title('Gaussian Blurred')
-plt.xticks([]), plt.yticks([])
-plt.show()
+	plt.subplot(151),plt.imshow(img1.astype('uint8')),plt.title('Original')
+	plt.xticks([]), plt.yticks([])
+	plt.subplot(152),plt.imshow(dblur.astype('uint8')),plt.title('Original 2.0')
+	plt.xticks([]), plt.yticks([])
+	plt.subplot(153),plt.imshow(gblur.astype('uint8')),plt.title('Gaussian Blurred')
+	plt.xticks([]), plt.yticks([])
+	plt.subplot(154),plt.imshow(logblur.astype('uint8')),plt.title('LoG Blurred')
+	plt.xticks([]), plt.yticks([])
+	plt.subplot(155),plt.imshow(mblur.astype('uint8')),plt.title('Mean Blurred')
+	plt.xticks([]), plt.yticks([])
+	plt.show()
+plot("1.jpg")

45c568d795823aa124d547476ff2f6b04f1cf66c
vedantdeo <vedantdeo1@gmail.com>
2022-03-21 17:55:32 +0530

    Start implementing LOG

--- convolution.py
+++ convolution.py
@@ -2,19 +2,7 @@ from random import gauss
 import numpy as np
 import math
 import cv2
-def evaluator(x,mean,sigma):
-    a = math.exp(-((((x-mean)/(sigma))**2)/2))    
-    return (1 / (np.sqrt(2 * np.pi) * sigma))*a
 
-
-def gaussian_kernel_generator(n,sigma): 
-    kernel_1D = np.linspace(-1*(n // 2), n // 2, n)
-    for i in range(n):
-        kernel_1D[i] = evaluator(kernel_1D[i], 0, sigma)
-    ## To generate a 2d gaussian kernel, we can take an outer product of the 1d kernel with itself
-    kernel_2D = np.outer(kernel_1D.T, kernel_1D.T) 
-    kernel_2D *= 1.0 / kernel_2D.sum() 
-    return kernel_2D
 
 
 def convolve(matrix, kernel):
@@ -56,19 +44,42 @@ def convolveRGB(tensor, kernel):
     return result
 
 
+#LOG------------------------------------------------------------------------
 
-def delta_kernel_generator(n):
+
+def  log_kernel(n,sigma):
+    return gaussian_kernel(n,sigma)
+
+
+#LOG --------------------------------------------------------------------
+
+
+def evaluator(x,mean,sigma):
+    a = math.exp(-((((x-mean)/(sigma))**2)/2))    
+    return (1 / (np.sqrt(2 * np.pi) * sigma))*a
+
+
+def gaussian_kernel(n,sigma): 
+    kernel_1D = np.linspace(-1*(n // 2), n // 2, n)
+    for i in range(n):
+        kernel_1D[i] = evaluator(kernel_1D[i], 0, sigma)
+    ## To generate a 2d gaussian kernel, we can take an outer product of the 1d kernel with itself
+    kernel_2D = np.outer(kernel_1D.T, kernel_1D.T) 
+    kernel_2D *= 1.0 / kernel_2D.sum() 
+    return kernel_2D
+
+def delta_kernel(n):
     filter = np.zeros(shape=(n, n))
     filter[1][1]=1
     return(filter)
-def mean_kernel_generator(n):
+def mean_kernel(n):
     filter = np.ones(shape=(n, n))/(n*n)
     return(filter)
 
 def filter(image,filtertype,n,*args):
     if(len(args)>0):
-        kernel={"Gaussian":gaussian_kernel_generator(n,args[0])}
+        kernel={"Gaussian":gaussian_kernel(n,args[0]),"LoG":log_kernel(n,args[0])}
     else:
-        kernel={"Delta":delta_kernel_generator(n),"Mean":mean_kernel_generator(n)}
+        kernel={"Delta":delta_kernel(n),"Mean":mean_kernel(n)}
     result = convolveRGB(image, kernel[filtertype])
     return(result)

a66957ef8513c3750c69f85f9b6aa432abcacd2a
vedantdeo <vedantdeo1@gmail.com>
2022-03-27 22:21:38 +0530

    hello

--- boxfilters.py
+++ boxfilters.py
@@ -4,19 +4,17 @@ import convolution
 def filters(imagefile):
 	img1 = cv2.imread(imagefile)
 	imagename=imagefile.split(".")[0]
-	logblur1=convolution.filter(img1,"LoG",5,0.6)
-	cv2.imwrite(imagename+"_log1.jpg",logblur1)
-	logblur2 =convolution.filter(img1,"LoG",13,0.67)
-	cv2.imwrite(imagename+"_log2.jpg",logblur2)
-	logblur3 =convolution.filter(img1,"LoG",3,0.5)
-	cv2.imwrite(imagename+"_log3.jpg",logblur3)
-	logblur4=convolution.filter(img1,"LoG",5,0.6)
-	cv2.imwrite(imagename+"_log4.jpg",logblur4)
-	logblur5 =convolution.filter(img1,"LoG",5,0.55)
-	cv2.imwrite(imagename+"_log5.jpg",logblur5)
-	logblur6 =convolution.filter(img1,"LoG",5,0.5)
-	cv2.imwrite(imagename+"_log6.jpg",logblur6)
-
+	typ="logsize"
+	logblur1=convolution.filter(img1,"LoG",3,1)
+	cv2.imwrite(imagename+"_"+typ+"1.jpg",logblur1)
+	logblur2 =convolution.filter(img1,"LoG",5,1)
+	cv2.imwrite(imagename+"_"+typ+"2.jpg",logblur2)
+	logblur3 =convolution.filter(img1,"LoG",7,1)
+	cv2.imwrite(imagename+"_"+typ+"3.jpg",logblur3)
+	logblur4=convolution.filter(img1,"LoG",9,1)
+	cv2.imwrite(imagename+"_"+typ+"4.jpg",logblur4)
+	logblur5 =convolution.filter(img1,"LoG",11,1)
+	cv2.imwrite(imagename+"_"+typ+"5.jpg",logblur5)
 
 	print(estimate_sigma(img1, channel_axis=-1, average_sigmas=True))
 	print(estimate_sigma(logblur1, channel_axis=-1, average_sigmas=True))
@@ -24,5 +22,4 @@ def filters(imagefile):
 	print(estimate_sigma(logblur3, channel_axis=-1, average_sigmas=True))
 	print(estimate_sigma(logblur4, channel_axis=-1, average_sigmas=True))
 	print(estimate_sigma(logblur5, channel_axis=-1, average_sigmas=True))
-	print(estimate_sigma(logblur6, channel_axis=-1, average_sigmas=True))
-filters("5.jpg")
+filters("1.jpg")
